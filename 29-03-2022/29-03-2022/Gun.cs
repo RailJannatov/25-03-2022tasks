64a975e8aec109c0f4a5810e6fa276d27deab494
RailJannatov <railcennetov@gmail.com>
2022-03-29 19:29:22 +0400

    tasks added

---
+++ 29-03-2022/29-03-2022/Gun.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _29_03_2022
+{
+    public class Gun
+    {
+        public int CurrentBullet;
+        public int Magazine;
+        public int MaxBulletCount;
+
+        public Gun(int currentBullet,int magazine,int maxBulletCount)
+        {
+            CurrentBullet = currentBullet;
+            Magazine = magazine;
+            MaxBulletCount = maxBulletCount;
+        }
+        public void Fire(string rejim)
+        {
+            if(rejim == "Single")
+            {
+                CurrentBullet--;
+            }
+            else if (rejim == "Automatic")
+            {
+                CurrentBullet = 0;
+            }
+            Console.WriteLine($"Qalan gulle: {CurrentBullet}");
+        }
+
+        public void Reload(int bullet)
+        {
+            if(bullet <= MaxBulletCount)
+            {
+                int BulletReload = CurrentBullet + bullet;
+                if (BulletReload <= Magazine)
+                {
+                    CurrentBullet += bullet;
+                    MaxBulletCount -= bullet;
+                    Console.WriteLine($"Daragdaki gullelerin sayi : {CurrentBullet} Bizde olan gullelerin sayi: {MaxBulletCount}");
+                }
+                else
+                {
+                    int extraBullet = BulletReload - Magazine;
+                    CurrentBullet = Magazine;
+                    MaxBulletCount -= bullet;
+                    MaxBulletCount += extraBullet;
+                    Console.WriteLine($"Daragda qalan gulle :{CurrentBullet} Elave qalan gullelerin sayi:{extraBullet} Bizde olan gullelerin sayi:{MaxBulletCount}");
+                }
+            }
+        }
+    }
+}
---
+++ 29-03-2022/29-03-2022/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _29_03_2022
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Gun gun1 = new Gun(20,30,100);
+            
+
+            gun1.Reload(20);
+        }
+    }
+}
