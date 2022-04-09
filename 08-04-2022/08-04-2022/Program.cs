b564e3b3764811be81e367c17f78949f53594bc7
RailJannatov <railcennetov@gmail.com>
2022-04-09 12:59:10 +0400

    tasks added

---
+++ 08-04-2022/08-04-2022/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _08_04_2022
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person Murad = new Person("Murad", "Abdullazade", 19);
+            Person Ibrahim = new Person("Ibrahim", "Isgenderov", 20);
+            Person ALi = new Person("Ali", "Veliyev", 19);
+            Person Gulchin = new Person("Gulchin", "Taghizade", 21);
+            Person Fidan = new Person("Fidan", "Memmedova", 19);
+            Person Samir = new Person("Samir", "Aliyev", 23);
+            Person Rail = new Person("Rail", "Jannatov", 24);
+
+            List<Person> people = new List<Person>();
+
+            people.Add(Murad);
+            people.Add(Ibrahim);
+            people.Add(ALi);
+            people.Add(Gulchin);
+            people.Add(Fidan);
+            people.Add(Samir);
+            people.Add(Rail);
+
+
+            //var human = people.Find(person => person.Age == 19);
+            //Console.WriteLine(human.Name);
+            //Console.WriteLine(people.Find(person => person.Age == 19).Name);
+
+            foreach (var item in people.FindAll(gunay=>gunay.Age==19))
+            {
+                Console.WriteLine(item.Name);
+            }
+         
+        }
+    }
+}
