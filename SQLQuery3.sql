b04c4d8c7ae1138d778e4bf771b97650adf323c7
RailJannatov <railcennetov@gmail.com>
2022-04-20 21:21:58 +0400

    sql tasks added

---
+++ SQLQuery3.sql
@@ -0,0 +1,9 @@
+/****** Script for SelectTopNRows command from SSMS  ******/
+SELECT TOP (1000) [Name]
+      ,[Surname]
+  FROM [PractiseP318].[dbo].[Person]
+
+  Insert into Person values ('Murad','Abdullayev')
+
+  SELECT * FROM Person
+  Select Name From Person WHERE Name='Murad'
